292dfe526fd91d4c93f1ebdde42d6351fa33404f
bichnln <bich.nln@gmail.com>
2021-01-23 22:24:08 +1100

    fix heroku dynamic port number problem

--- app.js
+++ app.js
@@ -18,12 +18,15 @@ const app = express();
 // passport config
 require('./config/passport')(passport);
 
+var port_number = server.listen(process.env.PORT || 3000);
+
+
 // connect to database
 const dbConn =
   "mongodb+srv://admin:KumonAshy16@students.qkpw0.mongodb.net/students?retryWrites=true&w=majority";
 mongoose
   .connect(dbConn, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(port_number))
   .catch((err) => console.log(err));
 
 // set view engine
